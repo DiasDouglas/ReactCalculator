dd6b4caed54aac3fd0f6c256f74844c3d37d84f0
Douglas Dias <douglas.dias@supertroco.com.br>
2020-04-15 01:38:19 -0300

    Finalizing the application

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -20,15 +20,75 @@ export default class Calculator extends Component {
         this.clearMemory = this.clearMemory.bind(this)
         this.setOperation = this.setOperation.bind(this)
         this.addDigit = this.addDigit.bind(this)
+        this.additiveInverse = this.additiveInverse.bind(this)
     }
 
     clearMemory(){
         this.setState({ ...initialState })
     }
 
+    additiveInverse(){
+        console.log(this.state)
+        const values = [...this.state.values]
+        const currentValueIndex = this.state.currentValueIndex
+
+        values[currentValueIndex] *= -1 
+
+        this.setState({ values: values, displayValue: values[currentValueIndex] })
+    }
+
     setOperation(operation){
         if(this.state.currentValueIndex === 0){
-            this.setState({operation, current: 1, clearDisplay: true})
+            this.setState({operation, currentValueIndex: 1, clearDisplay: true})
+        }
+        else {
+            const equals = operation === '='
+            var divisionByZero = false
+            const currentOperation = this.state.operation
+
+            const values = [...this.state.values]
+            
+            switch(currentOperation){
+                case '+':
+                    values[0] = values[0] + values[1]
+                    values[1] = 0
+                    break
+                case '-':
+                    values[0] = values[0] - values[1]
+                    values[1] = 0
+                    break
+                case 'x':
+                    values[0] = values[0] * values[1]
+                    values[1] = 0
+                    break
+                case '/':
+                    if(values[1] === 0){
+                        divisionByZero = true
+                    }
+                    else {
+                        values[0] = values[0] / values[1]
+                        values[1] = 0
+                    }
+                    break
+                case '%':
+                    values[0] = (values[0] / 100) * values[1]
+                    values[1] = 0
+                    break
+                default:
+            }
+
+            if(!divisionByZero){
+                this.setState({
+                    displayValue: values[0], 
+                    operation: equals ? null : operation,
+                    currentValueIndex: equals ? 0 : 1,
+                    clearDisplay: !equals,
+                    values
+                })
+            }
+            else {
+                this.setState({...initialState, displayValue: "ERROR"})
+            }
         }
     }
 
@@ -38,7 +98,7 @@ export default class Calculator extends Component {
         }
         
         const clearDisplay = this.state.displayValue === '0' ||
-            this.state.clearDisplay
+            this.state.displayValue === "ERROR" || this.state.clearDisplay
         
         const currentValue = clearDisplay ? '' : this.state.displayValue
         const displayValue = currentValue + n
@@ -58,8 +118,8 @@ export default class Calculator extends Component {
             <div className="calculator">
                 <Display value={this.state.displayValue} />
                 <Button label="AC" click={this.clearMemory}/>
-                <Button label="+/-"/>
-                <Button label= "%"/>
+                <Button label="+/-" click={this.additiveInverse}/>
+                <Button label= "%" click={this.setOperation}/>
                 <Button label="/" operation click={this.setOperation}/>
                 <Button label="7" click={this.addDigit}/>
                 <Button label="8" click={this.addDigit}/>
