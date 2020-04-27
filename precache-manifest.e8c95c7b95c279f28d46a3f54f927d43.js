1ae98de8ddf3075d6665663178e35a45a5751998
Douglas Dias <douglas.dias@supertroco.com.br>
2020-04-27 17:31:20 -0300

    Updates

---
+++ precache-manifest.e8c95c7b95c279f28d46a3f54f927d43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15bc88e30501891841a61980cbfa9142",
+    "url": "/ReactCalculator/index.html"
+  },
+  {
+    "revision": "262706174e00860654b0",
+    "url": "/ReactCalculator/static/css/main.74e0d509.chunk.css"
+  },
+  {
+    "revision": "6daaa1fd0e13f1727257",
+    "url": "/ReactCalculator/static/js/2.35405151.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactCalculator/static/js/2.35405151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "262706174e00860654b0",
+    "url": "/ReactCalculator/static/js/main.7e967db0.chunk.js"
+  },
+  {
+    "revision": "22a35caa3c7582105b52",
+    "url": "/ReactCalculator/static/js/runtime-main.64185f2f.js"
+  },
+  {
+    "revision": "288302ea531af8be59f6ac2b5bbbfdd3",
+    "url": "/ReactCalculator/static/media/RobotoMono-Thin.288302ea.ttf"
+  }
+]);
